1f0543dd20a219a82a2459c09a69a0b3e5848097
Ginkophyta <kittipong.miler@gmail.com>
2019-02-26 22:18:39 +0700

    2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -22,14 +22,39 @@ int main(){
 
 // Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
 void inputMatrix(double A[][N]){
-	for(int i=0;i<N;i++)
-	cout<<"Row "<<i+1<<": ";
+	for(int i=0;i<N;i++){
+		cout<<"Row "<<i+1<<": ";
+		for(int j=0;j<N;j++){
+			cin>>A[i][j];
+		}
+	}
+
 }
 
 void findLocalMax(const double A[][N], bool B[][N]){
-	
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(A[i][j]>=A[i][j-1] && A[i][j] >= A[i][j+1] && A[i][j] >= A[i-1][j] && A[i][j]>=A[i+1][j] ){
+				B[i][j]=1;
+			}else{
+				B[i][j]=0;
+			}
+		}
+	}
+	for(int i=0;i<N;i++){
+		
+		B[i][0]=0;
+		B[i][N-1]=0;
+		B[0][i]=0;
+		B[N-1][i]=0;	
+	} 
 }
 
 void showMatrix(const bool B[][N]){
-	
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout<<B[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
 }
