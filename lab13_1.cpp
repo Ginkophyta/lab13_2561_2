48c044d404921e7e10259d64aa5ad13dd8daaa9c
lab314-52@labeng.local <lab314-52@labeng.local>
2019-02-26 18:11:15 +0700

    1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -13,8 +13,37 @@ template <typename T>
 void insertionSort(T d[],int N){
 	
 //Write your code here.
-
+for(int i=1;i<N;i++){
+	int z;
+	for(int k=0;k<N;k++){
+		if(k==i){
+			cout<<"["<<d[k]<<"]"<<" ";
+		}else{
+		cout<<d[k]<<" ";	
+		}
+	}
+	cout<<"=> ";
+	z=i;
+    for (int j=i;j>0;j--){
+    	
+    	if(d[j]>d[j-1]){
+    	swap(d,j,j-1);
+		z=j-1;	
+		} 
+    	
+	}
+	for(int k=0;k<N;k++){
+		if(k==z){
+			cout<<"["<<d[k]<<"]"<<" ";
+		}else{
+		cout<<d[k]<<" ";	
+		}
+	}
+		cout<<"\n";
+	
 }
+}
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -27,3 +56,6 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+
+ 
